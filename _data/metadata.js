47921e776e982de0a069c64c134dbbbd459ac8a2
Ibrahima NDIAYE <ibrahima.ndiaye450@gmail.com>
2025-07-27 14:40:37 +0000

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Ibrahima's Personal Blog",
+	url: "https://ibraloop.dev/",
+	language: "en",
+	description: "Writing whenever I feel like it.",
+	author: {
+		name: "Ibrahima",
+		email: "ibraloop@gmail.com",
+		url: "https://ibraloop.dev"
+	}
+}
